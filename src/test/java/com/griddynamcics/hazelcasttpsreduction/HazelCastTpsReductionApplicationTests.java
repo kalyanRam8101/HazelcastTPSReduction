61ceafb4b1fac773cd54e10921c194e600e17660
Kalyan Ram <kram@griddynamics.com>
2026-02-18 13:05:49 +0530

    first commit

---
+++ src/test/java/com/griddynamcics/hazelcasttpsreduction/HazelCastTpsReductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.griddynamcics.hazelcasttpsreduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HazelCastTpsReductionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
