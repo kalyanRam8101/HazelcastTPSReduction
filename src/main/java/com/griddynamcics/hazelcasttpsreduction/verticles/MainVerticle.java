d0aedb9b66bc584c502cca4874e7a53546f46820
Kalyan Ram <kram@griddynamics.com>
2026-02-18 17:27:25 +0530

    changes

--- src/main/java/com/griddynamcics/hazelcasttpsreduction/verticles/MainVerticle.java
+++ src/main/java/com/griddynamcics/hazelcasttpsreduction/verticles/MainVerticle.java
@@ -114,7 +114,19 @@ public class MainVerticle extends AbstractVerticle {
 				})
 				.onFailure(error -> {
 					startPromise.fail(error);
-					log.error("Failed to start HTTP server", error);
+					log.error("Failed to start HTTP serve100
+										 
+						
+				
+					
+						 											
+					
+					 
+				
+							
+							
+							
+				", error);
 				});
 	}
 
